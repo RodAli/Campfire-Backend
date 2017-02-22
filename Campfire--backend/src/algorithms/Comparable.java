0f5c881b1baec1e21a8c3f3c2e8c98edc6c270c5
Vlad Chap <all4vlad@gmail.com>
2017-02-21 19:30:37 -0500

    Adding a new way of comparing categories - needs fix

---
+++ Campfire--backend/src/algorithms/Comparable.java
@@ -0,0 +1,11 @@
+package algorithms;
+
+/*
+ * The interface which all criteria need to implement.
+ * Allows comparisons between the criteria of two
+ * different students.
+ * 
+ */
+public interface Comparable {
+	public double Compare(Comparable other);
+}
